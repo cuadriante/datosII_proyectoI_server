c08294c6545daeb3537f44738bdc488ed784d3fe
cuadriante <adriana@sinapsis.ws>
2021-09-03 02:24:13 -0600

    initial server

---
+++ ServerSocket.h
@@ -0,0 +1,43 @@
+//
+// Created by cuadriante on 2/9/21.
+//
+
+#ifndef DATOSII_PROYECTOI_SERVER_SERVERSOCKET_H
+#define DATOSII_PROYECTOI_SERVER_SERVERSOCKET_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h> //librerias para red
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <pthread.h> // threads for multiprogramming
+#include <vector> //para almacenar varios clientes
+#include <unistd.h>
+using namespace std;
+
+//Structure to represent clients
+struct dataSocket{
+    int descriptor;
+    sockaddr_in info;
+};
+
+class ServerSocket {
+public:
+    ServerSocket();
+    void run();
+    void setMessage(const char* msn);
+
+private:
+    int descriptor; // se utiliza para identificar los sistemas
+    const int maxConnectedClients = 4;
+    static const int bufferSize = 1024;
+    sockaddr_in info;
+    vector<int> clients; // stores connected clients
+    bool create_socket();
+    bool bind_kernel(); //bind with port, establish maximum client quantity
+    static void* ClientController(void*);
+};
+
+
+#endif //DATOSII_PROYECTOI_SERVER_SERVERSOCKET_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include "ServerSocket.h"
+
+ServerSocket* server;
+void * serverRun(void *){
+    try {
+        server->run();
+    } catch (string ex) {
+        cout << ex << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    server = new ServerSocket;
+    pthread_t serverThread;
+    pthread_create(&serverThread, 0, serverRun, NULL);
+    pthread_detach(serverThread);
+
+    //Test: send messages from server to client
+    string json = "Holi soy el servidor."; // !! esto es lo que hay que poner como un json de verdad proximamente
+    while(1){
+        string msn;
+        cin >> msn;
+        if(msn == "exit"){
+            break;
+        }
+        server->setMessage(json.c_str());
+
+    }
+    delete server;
     return 0;
 }
