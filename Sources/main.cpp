d3d336b1cfed4d0d77cd6fbe734415bfde38278b
cuadriante <adriana@sinapsis.ws>
2021-09-29 02:39:39 -0600

    moved game loop functions

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,15 +1,7 @@
 #include "../Headers/Socket.h"
 #include "../Headers/ServerListener.h"
-#include "../Headers/Breakout/GameInfo.h"
 #include "../Headers/Breakout/GameModeSettings.h"
 #include "../Headers/Breakout/GameLoop.h"
-
-//void checkForBallOutOfBounds(Ball *ball);
-//
-//void checkBlockCollision(GameModeSettings gameModeSettings, GameInfo *gameInfo, Ball *ball);
-//
-//void checkPlayerBarCollision(const GameInfo *gameInfo, Ball *ball);
-
 
 
 
@@ -19,9 +11,6 @@ int main() {
     ServerListener serverListener;
     if (serverListener.start()) {
         GameLoop gameLoop(gameInfo);
-//        pthread_t thread;
-//        pthread_create(&thread, 0, gameLoop, (void *) gameInfo);
-//        pthread_detach(thread);
         serverListener.waitForConnections(gameInfo);
     }
     return 0;
